f1f5721066df2358ab4bc35fefc88cbccafa7250
Your Name <mailebai123@gmail.com>
2026-05-15 10:33:24 +0500

    Update UserMapper

--- src/main/java/com/example/demo/mapper/UserMaper.java
+++ src/main/java/com/example/demo/mapper/UserMaper.java
@@ -5,17 +5,14 @@ import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
-
 @Component
 public class UserMaper {
-
     public Map<String,Object> toMap(User user){
         Map<String,Object> map = new HashMap<>();
         map.put("id",user.getId());
         map.put("name",user.getName());
         map.put("email",user.getEmail());
         map.put("role",user.getRole());
-
         return map;
     }
 }
