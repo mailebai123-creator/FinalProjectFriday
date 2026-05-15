f25964723fc60d9e256f95d645349414519828fa
Your Name <mailebai123@gmail.com>
2026-05-15 10:23:54 +0500

    Update ResourceNotFoundException

--- src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -1,7 +1,5 @@
 package com.example.demo.exception;
 
 public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String massage){
-        super(massage);
-    }
+    public ResourceNotFoundException(String massage){super(massage);}
 }
